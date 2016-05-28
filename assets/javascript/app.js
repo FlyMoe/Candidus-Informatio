93ffb4456778759ee70e90b2717531a070823605
Jonathan Stewart <AnorLondo@Jonathans-MacBook-Air.local>
2016-05-28 09:58:05 -0400

    chart removed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -309,6 +309,7 @@ repubCounter = data.party_republican;
 console.log(demoCounter);
 otherCounter = data.party_Other;
 console.log(demoCounter);
+/*
 AmCharts.makeChart("chartdiv",
 		{
 			"type": "pie",
@@ -359,6 +360,7 @@ AmCharts.makeChart("chartdiv",
 			]
 		}
 	);
+	*/
 });
 
 // This function is for the voice recognition
